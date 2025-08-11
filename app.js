0d63b6784cd16cf04fbfea05db6a1619716840e7
olflin <raymond.13300@gmail.com>
2025-08-11 16:08:27 +0200

    suppression de app.use(express.static(path.join(__dirname, 'public')));

--- app.js
+++ app.js
@@ -54,7 +54,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // View engine setup (EJS)
 app.set('view engine', 'ejs');
